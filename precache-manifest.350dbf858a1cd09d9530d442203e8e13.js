c23689d78021e5a843967c3973c5b82f5349de47
Mohit Darmal <mohitdarmal1907@gmail.com>
2020-03-04 23:38:28 +0530

    Updates

---
+++ precache-manifest.350dbf858a1cd09d9530d442203e8e13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76dad3605d905d1f4ad531a208eac1bc",
+    "url": "/splash-pic-react/index.html"
+  },
+  {
+    "revision": "58de01eff091814ddaaf",
+    "url": "/splash-pic-react/static/js/2.9a9e0b76.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/splash-pic-react/static/js/2.9a9e0b76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "090e3a1f4c829b607672",
+    "url": "/splash-pic-react/static/js/main.6aeee783.chunk.js"
+  },
+  {
+    "revision": "0f1b1b9bc2ddc5359676",
+    "url": "/splash-pic-react/static/js/runtime-main.ea532408.js"
+  }
+]);
